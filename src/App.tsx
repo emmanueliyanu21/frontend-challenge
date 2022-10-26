38137ec449fc37ed20e1c83981a1c7c037395da1
Emmanuel <emmanuel@Emmanuels-MacBook-Pro.local>
2022-10-26 18:56:59 +0100

    creating financial card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,82 @@
-import { FinancialCard } from './component/FinancialCard';
-import family from './family.svg';
-import holiday from './going-holiday.svg';
-import retire from './retire.svg';
-import home from './home.svg';
-import emergency from './em-fund.svg';
-import debt from './debt.svg';
-import vision from './vision.svg';
+import { FinancialCard } from "./component/FinancialCard";
+import family from "./family.svg";
+import holiday from "./going-holiday.svg";
+import retire from "./retire.svg";
+import home from "./home.svg";
+import emergency from "./em-fund.svg";
+import debt from "./debt.svg";
+import vision from "./vision.svg";
 
 const familyData = {
   cardImage: family,
-  cardText: "Baby's birth"
-}
+  cardText: "Baby's birth",
+};
 
 const holidayData = {
   cardImage: holiday,
-  cardText: "Holiday"
-}
+  cardText: "Holiday",
+  text: 'In 3 years and 2 months'
+};
 
 const retirementData = {
   cardImage: retire,
-  cardText: "Retire"
-}
+  cardText: "Retire",
+};
 
 const newHomeData = {
   cardImage: home,
-  cardText: "New Home"
-}
+  cardText: "New Home",
+  text: 'In 1 year and 9 months'
+};
 
 const emergencyFundData = {
   cardImage: emergency,
-  cardText: "Emergency fund"
-}
+  cardText: "Emergency fund",
+  text: 'In 4 years and 9 months'
+};
 
 const debtData = {
   cardImage: debt,
-  cardText: "Debt free"
-}
+  cardText: "Debt free",
+  text: 'In 8 years and 11 months'
+};
 
 const contributionData = {
   cardImage: vision,
-  cardText: "Make a contribution to my community through philantrophy"
-}
+  cardText: "Make a contribution to my community through philantrophy",
+  text: 'Utimately'
+};
 
 function App() {
   return (
     <main>
-      
-     <FinancialCard {...familyData} />
-     <br />
-     <FinancialCard {...holidayData} />
-     <br />
-     <FinancialCard {...retirementData} />
-     <br />
-     <div className="line"></div>
-     <FinancialCard {...newHomeData} />
-     <br />
-     <FinancialCard {...emergencyFundData} />
-     <br />
-     <FinancialCard {...debtData} />
-     <br />
-     <FinancialCard {...contributionData} />
+      <div className="Wrapper">
+        <div className="WrapperContainer">
+          <div className="UpperCard">
+            <div className="CardItemOne">
+              <FinancialCard {...familyData} DisplayCircle='none' DisplayHolder = 'down' />
+              <FinancialCard {...holidayData} DisplayCircle='downCircle' DisplayHolder = 'down' />
+            </div>
+            <div className="CardItemThree">
+              <FinancialCard {...retirementData} DisplayCircle='none' DisplayHolder = 'down'   />
+            </div>
+          </div>
+          <div className="circle"></div>
+          <div className="line"></div>
+          <div className="line2"></div>
+          <div className="right"></div>
+          <div className="LowerCard">
+              <div className="CardItemLowerOne">
+                <FinancialCard {...newHomeData} />
+                <FinancialCard {...emergencyFundData} />
+              </div>
+              <div className="CardItemLowerTwo">
+                  <FinancialCard {...debtData} />
+                  <FinancialCard {...contributionData} color='opaqueBlue' DisplayCircle='none' />
+              </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- src/component/FinancialCard/FinancialCard.tsx
+++ src/component/FinancialCard/FinancialCard.tsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import styles from "./FinancialCard.module.css";
 
-import styles from './FinancialCard.module.css'
+type DisplayCircleType = "block" | "none" | "downCircle";
+type DisplayHolder = "up" | "down";
+type Displaycolor = "blue" | "opaqueBlue";
 
 type FinancialCardProps = {
-    cardImage: string
-    cardText: string
-  }
+  cardImage: string;
+  cardText: string;
+  text?: string;
+  DisplayCircle?: DisplayCircleType;
+  DisplayHolder?: DisplayHolder;
+  color?: Displaycolor;
+};
 
-const FinancialCard = ({cardImage,cardText}: FinancialCardProps): JSX.Element => 
-  (
-    <>
-        <div className={styles.FinancialCard}>
-          <img src={cardImage} alt="Family" />
-          <p style={{textAlign: "center"}}>{cardText}</p>
-     </div>
-    </>
-  )
+const FinancialCard = ({
+  text,
+  cardImage,
+  cardText,
+  DisplayCircle = "block",
+  DisplayHolder = "up",
+  color = "blue",
+}: FinancialCardProps): JSX.Element => (
+  <div>
+    <div className={`${styles.LineWrapper} ${styles[DisplayCircle]}`}>
+      <div className={styles.CircleBall}></div>
+    </div>
+    <div className={`${styles.Wrapper} ${styles[DisplayHolder]}`}>
+      <p className={styles.CardTextHeader}>{text}</p>
+    </div>
+    <div className={`${styles.LineWrapper} ${styles[DisplayHolder]}`}>
+      <div className={styles.ShortLine}></div>
+    </div>
+    <div className={`${styles.LineWrapper} ${styles[DisplayHolder]}`}>
+      <div className={`${styles.ArrowUp}`}></div>
+      <div className={`${styles.ArrowDown}`}></div>
+    </div>
+    <div className={`${styles.FinancialCard} ${styles[color]}`}>
+      <img src={cardImage} alt="Family" />
+      <p style={{ textAlign: "center" }}>{cardText}</p>
+    </div>
+  </div>
+);
 
-export { FinancialCard }
+export { FinancialCard };
